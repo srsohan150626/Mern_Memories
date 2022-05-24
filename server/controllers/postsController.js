707a7ff62f98add696812e897811dd1b73fb782d
srsohan150626 <sohan.ice.pust@gmail.com>
2022-05-24 17:19:02 +0600

    initial commit

---
+++ server/controllers/postsController.js
@@ -0,0 +1,14 @@
+import postModel from "../models/postModel.js";
+
+export  const getPosts = async (req,res) => {
+   try {
+       const posts = await postModel.find();
+       res.status(200).json(posts);
+   } catch (error) {
+       res.status(404).json({ message: error.message });
+   }
+};
+
+export  const createPost = (req,res) => {
+    res.send('Post Creation')
+};
