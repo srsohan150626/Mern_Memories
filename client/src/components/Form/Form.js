834f0c4877a9671ae71d11138b9eac6941690915
srsohan150626 <sohan.ice.pust@gmail.com>
2022-05-31 16:00:41 +0600

    create memory form complete with file upload

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,16 +1,26 @@
 import React, {useState} from "react";
 import useStyles from './styles';
-import {Paper, TextField, Typography} from "@material-ui/core";
+import {Button, Paper, TextField, Typography} from "@material-ui/core";
+import FileBase from 'react-file-base64';
+import {useDispatch} from "react-redux";
+import {createPost} from "../../actions/posts";
 
 const Form = () => {
     const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''})
     const classes = useStyles();
-    const handleSubmit = () => {
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createPost(postData));
+    }
+
+    const clear = () => {
 
     }
     return (
         <Paper className={classes.paper}>
-            <form className={classes.form} autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate onSubmit={handleSubmit}>
             <Typography variant="h6">Creating a Memory</Typography>
                 <TextField
                     name="creator"
@@ -44,6 +54,15 @@ const Form = () => {
                     value={postData.tags}
                     onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
                 />
+                <div className={classes.fileInput}>
+                    <FileBase
+                        type="file"
+                        multitple={false}
+                        onDone = {({base64}) => setPostData({ ...postData, selectedFile: base64 })}
+                    />
+                </div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit </Button>
+                <Button  variant="contained" color="secondary" size="small" onClick={clear} fullWidth> Clear </Button>
             </form>
         </Paper>
     );
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,10 +1,17 @@
 import React from "react";
 import useStyles from './styles';
+import {Card, CardMedia, Typography} from "@material-ui/core";
+import moment from "moment";
 
-const Post = () => {
+const Post = ({ post }) => {
     const classes = useStyles();
     return (
-        <h1>Post</h1>
+       <Card className={classes.card}>
+           <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay}>
+                <Typography variant="h6">{ post.creator }</Typography>
+            </div>
+       </Card>
     );
 }
 
